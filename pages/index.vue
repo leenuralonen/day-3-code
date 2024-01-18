63b8930afd9d804eda084f64b041703120ed2932
Leenur Alonen <923995@ACDM.DS.SAIT.CA>
2024-01-18 10:39:58 -0700

    got it

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
+<script setup>
+const { data: hello } = await useFetch("/api/hello");
+const { data } = await useFetch("/api/disney");
+const characters = data;
+const clientSideExample = "Updated Blah";
+</script>
+
 <template>
-  <Main>
-    <h1>
-      Hello World!
-    </h1>
-  </Main>
+  <main>
+    <header>
+      <h1>Hello CPRG</h1>
+    </header>
+
+    <section>
+      <h2>Hello Data Endpoint Example</h2>
+      <p>{{ hello.hello }}</p>
+      {{ clientSideExample }}
+    </section>
+    <section>
+      <h2>Disney Character Fetch</h2>
+      <ul>
+        <li v-for="character in characters.data" :key="character.id">
+          <h3>{{ character.name }}</h3>
+          <img :src="character.imageUrl" />
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
