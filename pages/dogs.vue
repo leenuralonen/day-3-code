73e476d0b498dc224e2762553c20a90f2e59c201
Leenur Alonen <923995@ACDM.DS.SAIT.CA>
2024-01-18 11:20:32 -0700

    for all the dawgs out there

---
+++ pages/dogs.vue
@@ -0,0 +1,20 @@
+<script setup>
+// fetch data
+const {data} = await useFetch("/api/dogs");
+const { _value: dataValues} = data;
+const {message: breeds} = dataValues;
+</script>
+
+<template>
+  <Main>
+    <header>
+    <h1>Dog breeds</h1>
+    </header>
+    <div>
+       <ul>
+        <pre> {{ breeds }}</pre>
+        <li v-for="breed in breeds">{{ breed }}</li>
+      </ul>
+    </div>
+  </Main>
+</template>
